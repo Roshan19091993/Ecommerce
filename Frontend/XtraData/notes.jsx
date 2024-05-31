97ac89cd81886cd8e30ead26facb87654f25e355
Your Name <you@example.com>
2024-05-31 09:59:29 +0530

    Remove XtraData folder from Frontend

--- Frontend/XtraData/notes.jsx
+++
@@ -1,7 +0,0 @@
-//Cart.js
-//VerticalCardProduct.js
-//HorizontalCardProduct.js
-//Inside helper/addTocart.js
-//App.js
-//all this having 
-//increment() ,decrement(), fetchuserAddToCart(),countCartProduct,setCountCartProduct
